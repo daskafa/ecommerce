c490ec6b0ba03e77466b963b6f68487ea34d5a44
yilmaz <ydaskafa@gmail.com>
2023-07-03 23:07:22 +0300

    HasFactory removed from models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    private $isNewSituations = [
+        0 => 'Not New',
+        1 => 'New',
+    ];
+
+    private $showStockCountsSituations = [
+        0 => 'Hide Stock Counts',
+        1 => 'Show Stock Counts',
+    ];
+
+    /**
+     * @return string[]
+     */
+    public function getIsNewSituations(): array
+    {
+        return $this->isNewSituations;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getShowStockCountsSituations(): array
+    {
+        return $this->showStockCountsSituations;
+    }
 }
